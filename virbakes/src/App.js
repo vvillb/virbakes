5528b96ed1dc428a861b1124eebba2ba24322139
Virinia Villalobos <virginiavt98@gmail.com>
2023-02-18 14:31:01 +0100

    structure
    base structure for the web, images

--- virbakes/src/App.js
+++ virbakes/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import React from 'react';
+import Header from './components/Header';
+import ProductList from './components/ProductList';
+import ContactForm from './components/ContactForm';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <main>
+        <ProductList />
+        <ContactForm />
+      </main>
+      <footer>
+        <p>&copy; 2023 virbakes</p>
+      </footer>
     </div>
   );
 }
---
+++ virbakes/src/components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header>
+      <h1>virbakes</h1>
+      <nav>
+        <ul>
+          <li><a href="/virbakes/src/components/Home.js">Inicio</a></li>
+          <li><a href="/virbakes/src/components/Catalog.js">Catálogo</a></li>
+          <li><a href="/virbakes/src/components/CustomizableProduct.js">Personalizables</a></li>
+          <li><a href="/virbakes/src/components/ContactForm.js">Contacto</a></li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
